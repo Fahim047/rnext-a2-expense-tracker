2cb1689863a741d62dfe7bb8f16e2ef4172d78e9
Fahim047 <fahimulislam58@gmail.com>
2024-10-14 06:43:19 +0600

    💄 Header Component Design Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Header from './components/Header';
+
 function App() {
 	return (
-		<div className="flex justify-center items-center w-full h-screen bg-gray-900">
-			<h1 className="text-3xl font-bold text-center text-blue-400">
-				Vite + React + Tailwind CSS
-			</h1>
-		</div>
+		<>
+			<Header />
+		</>
 	);
 }
 
